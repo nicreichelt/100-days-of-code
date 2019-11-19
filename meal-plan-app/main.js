962135a0ab9b89f850f8f267be8d42dd5f771970
Nic Reichelt <nic.reichelt@gmail.com>
2019-11-19 07:16:12 -0800

    100 Days of Code: Day 3 - Added click functionality to all "main" buttons - Adjusted animation of "show" so looks like a fade out/in - Added placeholder text to each section

--- meal-plan-app/main.js
+++ meal-plan-app/main.js
@@ -2,26 +2,43 @@ window.onload = function(){
 
     // ** Global Variables
     // Page Elements
+    // Main Container & Section Heading
+    const mainContainer = document.querySelector('.container');
+    const sectionHeading = document.querySelector('#section-heading');
+    // Section COntainer
     const homeContainer = document.querySelector('.home');
-    const mealPlanContainer = document.querySelector('.meal-plan')
+    const mealPlanContainer = document.querySelector('.meal-plan');
+    const mealsReferenceCOntainer = document.querySelector('.meals-reference');
+    const remindersContainer = document.querySelector('.reminders');
+    const settingsContainer = document.querySelector('.settings');
+
     // Meal Plan Options
     const mealPlanOptions = [
         {
             name: "Meal Plan",
-            section: "meal-plan"
+            section: "meal-plan",
+            container: "mealPlanContainer"
         },
         {
             name: "Meals Reference",
-            section: "meals-reference"
+            section: "meals-reference",
+            container: "mealsReferenceContainer"
         },
         {
             name: "Reminders",
-            section: "reminders"
+            section: "reminders",
+            container: "remindersContainer"
         },
         {
             name: "Settings",
-            section: "settings"
+            section: "settings",
+            container: "settingsContainer"
         },
+        {
+            name: "Simple Meal Planning!",
+            section: "home",
+            container: "homeContainer"
+        }
     ];
     // Home Plan Button = 
     const home = {
@@ -33,73 +50,144 @@ window.onload = function(){
     // Create App Options (Main Page)
     function loadMainPage(){
         mealPlanOptions.forEach(option => {
-            // Create Button
-            mealPlanButton = document.createElement('button');
-            mealPlanButton.classList.add('main-button');
-            mealPlanButton.classList.add('transition');
-            mealPlanButton.textContent = option.name;
-            mealPlanButton.dataset.section = option.section;
+            if(option.section != 'home'){
+                // Create Button
+                mealPlanButton = document.createElement('button');
+                mealPlanButton.classList.add('main-button');
+                mealPlanButton.classList.add('transition');
+                mealPlanButton.textContent = option.name;
+                mealPlanButton.dataset.section = option.section;
 
-            mealPlanButton.addEventListener('click', goToSection)
-            
-            // Append Button to Container
-            homeContainer.appendChild(mealPlanButton);
+                mealPlanButton.addEventListener('click', goToSection)
+                
+                // Append Button to Container
+                homeContainer.appendChild(mealPlanButton);
+            }
         });
     }
 
-    // ** Utility Functions
-    // Go to section function
+    // ** Load Sections
+    // Go to section function - hides all divs and loads "shows" the target div
     function goToSection(e){
-        // Hide Home Container
-        homeContainer.classList.add('hide');
-
         // Hide All "Section" divs
-        const divs = homeContainer.querySelectorAll('div');
+        const divs = mainContainer.querySelectorAll('div');
         divs.forEach(div => {
             div.classList.remove('show');
+            div.style.zIndex = -10;
         })
 
         // Show selected "Section" div
         const section = document.querySelector(`.${e.target.dataset.section}`);
-        section.classList.add('show');
+        // Delay screen load by 0.5s
+        setTimeout(() => {
+            section.classList.add('show');
+            section.style.zIndex = 10;
+        }, 500);
 
         // Populate Section
         switch(`${e.target.dataset.section}`){
             case "meal-plan": 
-                loadMealPlan();
+                loadMealPlanSection();
                 break;
             case "meals-reference":
-                loadMealsReference();
+                loadMealsReferenceSection();
                 break;
             case "reminders":
-                loadReminder();
+                loadRemindersSection();
                 break;
             case "settings":
-                loadSettings();
+                loadSettingsSection();
                 break;
-            default:
+            case "home":
                 goHome();
+                break;
         }
     }
     // Load Home Section
     function goHome(){
         console.log(home.section);
+        sectionHeading.textContent = 'Meal Planning';
+        const index = mealPlanOptions.map(option => option.section).indexOf('home');
+        const home = mealPlanOptions[index];
     }
+
     // Populate Meal Plan Section
-    function loadMealPlan(){
+    function loadMealPlanSection(){
         console.log('Meal Plan Section Loaded!');
+        // 
+        sectionHeading.textContent = 'The Meal Plan!';
+        // Go Home Button
+        if(!mealPlanContainer.querySelector('button.main-button')){
+            // Generate "Home" Button
+            let goHome = generateHomeButton();
+            goHome.addEventListener('click', goToSection);
+            mealPlanContainer.appendChild(goHome);
+        }else{}
+        // Placeholder Data
+        loadPlaceHolderText(mealPlanContainer , 'Meal Plan data goes here...');
     }
-    // Populate Meal Plan Section
-    function loadMealsReference(){
+    // Populate Meals Reference Section
+    function loadMealsReferenceSection() {
         console.log('Meals Reference Section Loaded!');
+        sectionHeading.textContent = 'Meals Reference';
+        // Go Home Button
+        if (!mealsReferenceCOntainer.querySelector('button.main-button')) {
+            // Generate "Home" Button
+            let goHome = generateHomeButton();
+            goHome.addEventListener('click', goToSection);
+            document.querySelector('.meals-reference').appendChild(goHome);
+        }
+        // Placeholder Data
+        loadPlaceHolderText(mealsReferenceCOntainer , 'Meals Reference data goes here...');
     }
-    // Populate Meal Plan Section
-    function loadReminder(){
+    // Populate Reminders Section
+    function loadRemindersSection() {
         console.log('Reminders Section Loaded!');
+        sectionHeading.textContent = 'Reminders';
+        // Go Home Button
+        if (!remindersContainer.querySelector('button.main-button')) {
+            // Generate "Home" Button
+            let goHome = generateHomeButton();
+            goHome.addEventListener('click', goToSection);
+            document.querySelector('.reminders').appendChild(goHome);
+        }
+        // Placeholder Data
+        loadPlaceHolderText(remindersContainer, 'Reminders data goes here...');
     }
-    // Populate Meal Plan Section
-    function loadSettings(){
+    // Populate Settings Section
+    function loadSettingsSection() {
         console.log('Settings Section Loaded!');
+        sectionHeading.textContent = 'Settings';
+        // Go Home Button
+        if (!settingsContainer.querySelector('button.main-button')) {
+            // Generate "Home" Button
+            let goHome = generateHomeButton();
+            goHome.addEventListener('click', goToSection);
+            document.querySelector('.settings').appendChild(goHome);
+        }
+        // Placeholder Data
+        loadPlaceHolderText(settingsContainer, 'Settings data goes here...');
+    }
+
+    // ** Utility Functions
+    // Generate Home Button
+    function generateHomeButton(){
+        mealPlanButton = document.createElement('button');
+        mealPlanButton.classList.add('main-button');
+        mealPlanButton.classList.add('transition');
+        mealPlanButton.textContent = 'Go Home';
+        mealPlanButton.dataset.section = home.section;
+
+        mealPlanButton.addEventListener('click', goHome)
+
+        return mealPlanButton;
+    }
+    // Load Placeholder text
+    function loadPlaceHolderText(cont, text){
+        const p = document.createElement('p');
+        const t  = document.createTextNode(text);
+        p.appendChild(t);
+        cont.appendChild(p);
     }
 
     // ** Event Listeners
